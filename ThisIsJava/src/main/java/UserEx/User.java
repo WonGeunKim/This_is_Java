06479ca26676a78b3b8f064738cfdf1414762685
WonGeunKim <kwg4764@naver.com>
2023-02-03 01:50:27 +0900

    - UserEx

---
+++ ThisIsJava/src/main/java/UserEx/User.java
@@ -0,0 +1,69 @@
+package UserEx;
+
+import java.util.Objects;
+
+public class User {
+
+	private String userId;
+	private String userName;
+	private String userPassword;
+	private int userAge;
+	private String userEmail;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public int getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(userAge, userEmail, userId, userName, userPassword);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return userAge == other.userAge && Objects.equals(userEmail, other.userEmail)
+				&& Objects.equals(userId, other.userId) && Objects.equals(userName, other.userName)
+				&& Objects.equals(userPassword, other.userPassword);
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", userPassword=" + userPassword + ", userAge="
+				+ userAge + ", userEmail=" + userEmail + "]";
+	}
+	
+	
+	
+}
