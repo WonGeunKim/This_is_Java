89c516a33c88b0a1b0561c5118915977b51ef70b
WonGeunKim <kwg4764@naver.com>
2023-02-03 21:33:59 +0900

    - Swing & AWT

---
+++ ThisIsJava/src/main/java/AWT/AppA.java
@@ -0,0 +1,43 @@
+package AWT;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+// Frame 상속
+public class AppA extends Frame {
+
+	public AppA() {
+		
+		// 제목 설정
+		setTitle("AWT App");
+		
+		// 윈도우 크기 설정
+		setSize(300, 100);
+		
+		// Button 추가
+		add(new Button("OK"), BorderLayout.SOUTH);
+		
+		// 윈도우 종료 버튼을 클릭하면 프로세스 종료
+		addWindowListener(new WindowAdapter() {
+			
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		// 윈도우 생성
+		AppA app = new AppA();
+		
+		// 윈도우를 보여줌
+		app.setVisible(true);
+	}
+
+}
---
+++ ThisIsJava/src/main/java/Swing/AppS.java
@@ -0,0 +1,55 @@
+package Swing;
+
+import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+// JFrame 상속
+public class AppS extends JFrame {
+	
+	public AppS() {
+		
+		// 제목 설정
+		setTitle("Swing App");
+		
+		// 윈도우 크기 설정
+		setSize(300, 100);
+		
+		// JButton 추가
+		getContentPane().add(new JButton("OK"), BorderLayout.SOUTH);
+		
+		// 윈도우 종료 버튼을 클릭하면 프로세스 종료
+		addWindowListener(new WindowAdapter() {
+		
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		
+		// 이벤트 큐에 Runnable 넣기
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				// 윈도우 생성
+				AppS app = new AppS();
+				
+				// 윈도우를 보여줌
+				app.setVisible(true);
+				System.out.println(Thread.currentThread().getName());
+				
+			}
+		});
+		
+	}
+
+}
