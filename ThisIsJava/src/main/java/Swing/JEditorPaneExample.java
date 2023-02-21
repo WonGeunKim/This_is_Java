32ac3b939137952d95f54be6970e10a3e8c360d0
WonGeunKim <kwg4764@naver.com>
2023-02-21 21:53:14 +0900

    - JEditorPane Ex

---
+++ ThisIsJava/src/main/java/Swing/JEditorPaneExample.java
@@ -0,0 +1,68 @@
+package Swing;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.io.IOException;
+
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+
+public class JEditorPaneExample extends JFrame{
+
+	private JEditorPane jEditorPane;
+	
+	// 메인 윈도우 설정
+	public JEditorPaneExample() {
+		this.setTitle("JEditorPaneExample");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.getContentPane().add(new JScrollPane(getJEditorPane()), BorderLayout.CENTER);
+		this.setSize(400, 300);
+	}
+	
+	
+	// JEditorPane 생성
+	public JEditorPane getJEditorPane() {
+
+		if(jEditorPane == null) {
+			jEditorPane = new JEditorPane();
+			
+			try {
+				jEditorPane.setPage(getClass().getResource("jeditor.html"));
+			} catch (Exception e) {
+			}
+			jEditorPane.setEditable(false);
+			jEditorPane.addHyperlinkListener(new HyperlinkListener() {
+				
+				@Override
+				public void hyperlinkUpdate(HyperlinkEvent e) {
+					
+					if(e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+						try {
+							jEditorPane.setPage(e.getURL());
+						} catch (IOException e2) {
+						}
+					}
+				}
+			});
+		}
+		
+		return jEditorPane;
+	}
+
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				JEditorPaneExample jFrame = new JEditorPaneExample();
+				jFrame.setVisible(true);
+			}
+		});
+	}
+	
+}
